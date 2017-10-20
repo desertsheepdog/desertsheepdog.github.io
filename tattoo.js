3841d0a070445451f192c90bdffa41235f0347ed
desertsheepdog <32944718+desertsheepdog@users.noreply.github.com>
2017-10-19 17:19:49 -0700

    Version 1

---
+++ tattoo.js
@@ -0,0 +1,26 @@
+/*
+	CIS166AA: Tattoo JavaScript
+	Author: David Goodyke
+	Date: 10/19/2017
+*/
+
+/*
+	The below variables are declared and defined for each 
+	image referencing their location to be used with the 
+	onclick events on artist page.
+*/
+var artistImage1 = "images/artistone.png";
+var artistImage2 = "images/artisttwo.png";
+var artistImage3 = "images/artistthree.png";
+var artistImage4 = "images/artistfour.png";
+
+/*
+	The below variables are declared and defined for each of the
+	artist's names, the strings can be changed over time
+	as the employment of different artists change.
+*/
+
+var artist1 = "Artist One"
+var artist2 = "Artist Two"
+var artist3 = "Artist Three"
+var artist4 = "Artist Four"
