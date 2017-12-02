dbc367e9dba3854c63524082a4b383fb67affb9e
desertsheepdog <32944718+desertsheepdog@users.noreply.github.com>
2017-12-02 12:32:01 -0700

    api change

--- weather.php
+++ weather.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "http://api.openweathermap.org/data/2.5/weather?lat=" . $_GET["lat"] . "&lon=" . $_GET["lng"] . "&mode=xml&APPID=760ce3d4a8ca81faf2c3dfb1fee3c583";
+$WeatherSource = "https://api.openweathermap.org/data/2.5/weather?lat=" . $_GET["lat"] . "&lon=" . $_GET["lng"] . "&mode=xml&APPID=760ce3d4a8ca81faf2c3dfb1fee3c583";
 header("Content-Type: application/xml");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
