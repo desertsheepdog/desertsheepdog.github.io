b02170014505b6c226a4c845588c6f63029b08dc
desertsheepdog <32944718+desertsheepdog@users.noreply.github.com>
2017-12-01 20:39:18 -0700

    api changes

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict"
 
 var waitForUser;
@@ -46,6 +45,7 @@ function getWeather() {
    httpRequest.abort();
    httpRequest.open("get","weather.php?" + "lat=" + currentLatitude + "&lon=" + currentLongitude, true);
    httpRequest.send(null);
+   console.log(httpRequest);
 }
 
 function loader(){
@@ -56,63 +56,4 @@ if (window.addEventListener) {
    window.addEventListener("load", loader, false);
 } else if (window.attachEvent) {
    window.attachEvent("onload", loader);
-=======
-"use strict"
-
-var waitForUser;
-var currentLatitude;
-var currentLongitude;
-var weatherReport;
-var httpRequest = false;
-var key = "760ce3d4a8ca81faf2c3dfb1fee3c583";
-
-function startGeolocation() {
-	waitForUser = setTimeout(fail, 10000);
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(getLocation, fail, {timeout: 10000});
-	} else {
-		fail();
-	}
-}
-
-function getLocation(position) {
-	clearTimeout(waitForUser);
-	currentLatitude = position.coords.latitude;
-	currentLongitude = position.coords.longitude;
-	getWeather();
-}
-
-function fail() {
-	console.log("Geolocation information not available or not authorized.");
-}
-
-function getRequestObject() {
-	try {
-		httpRequest = new XMLHttpRequest();
-	}
-	catch (requestError) {
-		console.log("Forecast not supported by your browser.");
-		return false;
-	}
-	return httpRequest;
-}
-
-function getWeather() {
-   if(!httpRequest) {
-	   httpRequest = getRequestObject();
-   }
-   httpRequest.abort();
-   httpRequest.open("get","weather.php?" + "lat=" + currentLatitude + "&lon=" + currentLongitude, true);
-   httpRequest.send(null);
-}
-
-function loader(){
-	startGeolocation();
-}
-
-if (window.addEventListener) {
-   window.addEventListener("load", loader, false);
-} else if (window.attachEvent) {
-   window.attachEvent("onload", loader);
->>>>>>> 57feae24864fbab8c2b9083103f911422ccdac6f
 }
--- weather.php
+++ weather.php
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 <?php
 $WeatherSource = "api.openweathermap.org/data/2.5/weather?lat=" . $_GET["currentLatitude"] . "&lon=" . $_GET["currentLongitude"] . "&mode=xml&APPID=760ce3d4a8ca81faf2c3dfb1fee3c583";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
-=======
-<?php
-$WeatherSource = "api.openweathermap.org/data/2.5/weather" . $_GET["lat="] . "&" . $_GET["lon="] . "&mode=xml&APPID=760ce3d4a8ca81faf2c3dfb1fee3c583";
-header("Content-Type: application/json");
-header("Cache-Control: no-cache");
-readfile($WeatherSource);
->>>>>>> 57feae24864fbab8c2b9083103f911422ccdac6f
 ?>
