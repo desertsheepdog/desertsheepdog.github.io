5d85488c7cbd661d5c2a06063b6f14846b106163
desertsheepdog <32944718+desertsheepdog@users.noreply.github.com>
2017-12-07 14:51:15 -0700

    php change to json

--- php/forecast.php
+++ php/forecast.php
@@ -1,6 +1,6 @@
 <?php
 $ForecastSource = "http://api.wunderground.com/api/4a14b4e3dca2bcbd/geolookup/conditions/forecast/astronomy/q/" . $_GET["lat"] . "," . $_GET["lon"] . ".json";
-header("Content-Type: application/html");
+header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($ForecastSource);
 ?>
