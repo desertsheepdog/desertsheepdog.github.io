01b93c1b9e7cf9572de67f0cea04986bdb85e28e
desertsheepdog <32944718+desertsheepdog@users.noreply.github.com>
2017-12-07 17:16:40 -0700

    x

--- forecast.php
+++ forecast.php
@@ -2,5 +2,5 @@
 $ForecastSource = "http://api.wunderground.com/api/4a14b4e3dca2bcbd/geolookup/conditions/forecast/astronomy/q/" . $_GET["lat"] . "," . $_GET["lon"] . ".json";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
-readfile($ForecastSource);
+echo ${ForecastSource};
 ?>
